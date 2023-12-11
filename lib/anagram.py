6c2281f54a84997081b49cead41f4d9eef90ffde
KennedyShiku <kenrichshiku@gmail.com>
2023-12-11 11:57:21 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if self._is_anagram(anagram)]
+
+    def _is_anagram(self, candidate):
+        candidate_lower = candidate.lower()
+        return sorted(candidate_lower) == sorted(self.word) and candidate_lower != self.word
